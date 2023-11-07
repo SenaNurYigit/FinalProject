f195e6c436fd399fac79416733c5fb8c27b46835
Sena Nur YİĞİT <sena.yigit@dmo.gov.tr>
2023-11-07 14:25:19 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        //Bu constructor sayesinde ProductManager bir yerde new'lendiğinde bana bir tane IProductDal referansı gönder demiş oluyoruz. IProductDal nesenesi de bizim DataAccess katmanımızdaki Interface.
+        //Böylece ilerde biz istediğimiz veri erişim metodunu kullanabiliyor olacağız.
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş kodları
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Category: IEntity
+    {
+        //Bu mimaride biz IEntity görünce bu classın bir veri tabanı tablosuna karşılık gelidiğini anlamış oluyoruz.
+        //Category sınıfı IEntity sınıfından miras aldığı için IEntity sınıfı bu sınıfın referansını tutmuş oluyor.
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
